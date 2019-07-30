f10f59b31ba1426f61618f04ad6cd0479be06760
Redaloukil <loukil.red@gmail.com>
2019-07-30 18:50:17 +0100

    Add linear gradient on home header

--- frontend/src/components/Home/Header.js
+++ frontend/src/components/Home/Header.js
@@ -6,7 +6,7 @@ const Header =  ({ appName, token }) => {
     <header className="header">
         <div id="content">
           <h1>Helping,Healing,Caring</h1>
-          <p>Search for the keywords to learn more about each warning.</p>
+          <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble</p>
           <a className="action">Join our community</a>
           <div className="roles">
             <div className="container">
